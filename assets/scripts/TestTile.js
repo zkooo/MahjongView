169d9e35f753c960bce92ab2865336cb50fec675
cavan <764918459@qq.com>
2020-08-12 15:25:34 +0800

    fix: 24 号碰牌

--- assets/scripts/TestTile.js
+++ assets/scripts/TestTile.js
@@ -6,13 +6,14 @@ cc.Class({
         tileText: cc.Sprite
     },
 
-    init (tileSF, textSF, textTrans) {//{scale, skew, position}
+    init(tileSF, textSF, textTrans) { //{scale, skew, position}
         this.tileBody.spriteFrame = tileSF;
         this.tileText.spriteFrame = textSF;
+        this.tileBody.node.scale = 720 / 1080;
         this.tileText.node.position = textTrans.position;
-        this.tileText.node.scale = textTrans.scale;        
+        this.tileText.node.scale = textTrans.scale // cc.pMult(textTrans.scale, (1080 / 720)) // cc.Vec2(textTrans.scale.x * (1080 / 720), textTrans.scale.y * (1080 / 720)) // cc.pMult(textTrans.scale, Math.pow((1080 / 720), 1)) // textTrans.scale * (1080 / 720);
         this.tileText.node.rotation = textTrans.rotation;
-        this.tileText.node.skewX = textTrans.skew.x;        
-        this.tileText.node.skewY = textTrans.skew.y;        
+        this.tileText.node.skewX = textTrans.skew.x;
+        this.tileText.node.skewY = textTrans.skew.y;
     }
 });
